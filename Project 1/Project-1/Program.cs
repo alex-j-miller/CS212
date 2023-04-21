3ce78c61d69836a36c1b22280a9f5d2a0e7c707d
Alex <100050360+xMelon74@users.noreply.github.com>
2023-04-21 02:15:45 -0400

    Add files via upload

---
+++ Project 1/Project-1/Program.cs
@@ -0,0 +1,41 @@
+﻿// Alex Miller
+// 9/15/2022
+// Log base 2 finder
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Fib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Log Base 2");
+            while (true)
+            {
+                Console.Write("\nEnter N: ");
+                long n = long.Parse(Console.ReadLine());
+                //long fib = Fib(n);
+                long fib = lg(lg(n));
+                Console.WriteLine("Floor of lg(lg({0})) = {1}.", n, fib);
+            }
+        }
+        static long Fib(long n)
+        {
+            if (n <= 2)
+                return 1;
+            else
+                return Fib(n - 1) + Fib(n - 2);
+        }
+
+        static long lg(long n)
+        {
+            if (n > 1)
+                return 1 + lg(n / 2);
+            else
+                return 0;
+        }
+    }
+}
